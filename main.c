a7a4ccb730f63f608e479e6500441ac62e8ac794
Deus77777 <118898059+Deus77777@users.noreply.github.com>
2022-12-08 14:32:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "joueur.h"
 #include "adversaire.h"
 #include "arbitre.h"
 #include "echecmat.h"
 #include "joueur2.h"
+#include "echecetmatjoueur2.h"
+
 
 int main()
 {
-
+    /*
     int echiquier[8][8]={{-5,-4,-3,-10,-19,-3,-4,-5},
                          {-1,-1,-1,-1  ,-1 ,-1,-1,-1},
                          { 0, 0, 0, 0  ,0 , 0, 0 , 0},
@@ -20,9 +25,20 @@ int main()
     queladversaire();
 
     tourjoueur(echiquier);
+    */
+
+    int echiquier[8][8]={{ 0, 0, 0, 0,  0,  0,  0 , 0},
+                         { 0, 0, 0, 0,  0,  0,  0 , 0},
+                         { 0, 0, 0, 0  ,0 , 0, 0 , 0},
+                         { 0, 0, 0, 0  ,0 , 0, 0 , 0},
+                         { 0, 0, 0, 0  ,0 , 0, 0 , 0},
+                         { 0, 0, 0, 0  ,0 , 0, 0 , 0},
+                         { 0, 1, 0, 0  ,1 , 0, 0, 0},
+                         { 0, 0, 0, 0,  0,  0,  0 , 0}};
+
+    tourIA(echiquier);
 
 
 
     return 0;
 }
-
